f4766ce2bf08e878d58d13c03b40139498aa7843
Jac Desktop <jlg17@sfu.ca>
2025-09-14 15:24:52 -0700

    Initial project commit

---
+++ BlueHarvest/Source/BlueHarvest/BlueHarvest.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BlueHarvest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BlueHarvest, "BlueHarvest" );
