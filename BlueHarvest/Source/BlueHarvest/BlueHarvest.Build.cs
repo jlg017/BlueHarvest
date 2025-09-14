4ad5e1f4f190d4d1c759b04de42e02ba22b24e2f
Jac Desktop <jlg17@sfu.ca>
2025-09-14 16:16:52 -0700

    Enable online subsystem and steam

--- BlueHarvest/Source/BlueHarvest/BlueHarvest.Build.cs
+++ BlueHarvest/Source/BlueHarvest/BlueHarvest.Build.cs
@@ -8,7 +8,7 @@ public class BlueHarvest : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "OnlineSubsystem", "OnlineSubsystemSteam" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
