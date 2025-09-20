9e152fb59ad1727e17467a9ee3689c0338c9aab0
Jac Desktop <jlg17@sfu.ca>
2025-09-20 15:50:14 -0700

    Added FirstPersonCharacter with basic movement

---
+++ BlueHarvest/Source/BlueHarvest/FirstPersonCharacter.cpp
@@ -0,0 +1,115 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FirstPersonCharacter.h"
+
+// Sets default values
+AFirstPersonCharacter::AFirstPersonCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Create a first person camera component.
+	FPCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	check(FPCameraComponent != nullptr);
+
+	// Attach the camera component to our capsule component.
+	FPCameraComponent->SetupAttachment(CastChecked<USceneComponent, UCapsuleComponent>(GetCapsuleComponent()));
+
+	// Position the camera slightly above the eyes.
+	FPCameraComponent->SetRelativeLocation(FVector(0.0f, 0.0f, 50.0f + BaseEyeHeight));
+
+	// Enable the pawn to control camera rotation.
+	FPCameraComponent->bUsePawnControlRotation = true;
+
+	// Create a first person mesh component for the owning player.
+	FirstPersonMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FirstPersonMesh"));
+	check(FirstPersonMesh != nullptr);
+
+	// Only the owning player sees this mesh.
+	FirstPersonMesh->SetOnlyOwnerSee(true);
+
+	// Attach the FPS mesh to the FPS camera.
+	FirstPersonMesh->SetupAttachment(FPCameraComponent);
+
+	// Disable some environmental shadows to preserve the illusion of having a single mesh.
+	FirstPersonMesh->bCastDynamicShadow = false;
+	FirstPersonMesh->CastShadow = false;
+
+	// The owning player doesn't see the regular (third-person) body mesh.
+	GetMesh()->SetOwnerNoSee(true);
+}
+
+// Called when the game starts or when spawned
+void AFirstPersonCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	check(GEngine != nullptr);
+
+	// Display a debug message for five seconds. 
+	// The -1 "Key" value argument prevents the message from being updated or refreshed.
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("We are using FPSCharacter."));
+}
+
+// Called every frame
+void AFirstPersonCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AFirstPersonCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// Set up "movement" bindings.
+	// TODO: Update to use Enhanced Input
+	PlayerInputComponent->BindAxis("MoveForward", this, &AFirstPersonCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AFirstPersonCharacter::MoveRight);
+
+	// Set up "look" bindings.
+	PlayerInputComponent->BindAxis("Turn", this, &AFirstPersonCharacter::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &AFirstPersonCharacter::AddControllerPitchInput);
+
+	// Set up "action" bindings.
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &AFirstPersonCharacter::JumpButtonPressed);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &AFirstPersonCharacter::JumpButtonReleased);
+
+	PlayerInputComponent->BindAction("Crouch", IE_Pressed, this, &AFirstPersonCharacter::CrouchButtonPressed);
+}
+
+void AFirstPersonCharacter::MoveForward(float Value)
+{
+	// Find out which way is "forward" and record that the player wants to move that way.
+	FVector Direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::X);
+	AddMovementInput(Direction, Value);
+}
+
+void AFirstPersonCharacter::MoveRight(float Value)
+{
+	// Find out which way is "right" and record that the player wants to move that way.
+	FVector Direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::Y);
+	AddMovementInput(Direction, Value);
+}
+
+void AFirstPersonCharacter::JumpButtonPressed()
+{
+	bPressedJump = true;
+}
+
+void AFirstPersonCharacter::JumpButtonReleased()
+{
+	bPressedJump = false;
+}
+
+void AFirstPersonCharacter::CrouchButtonPressed()
+{
+	if (bIsCrouched) {
+		UnCrouch();
+	}
+	else {
+		Crouch();
+	}
+}
